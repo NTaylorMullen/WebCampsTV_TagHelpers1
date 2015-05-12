59efbc1c1c75469b4329fe54ca090d585db34ec7
N. Taylor Mullen <nimullen@microsoft.com>
2015-05-12 16:12:45 -0700

    Initial content commit.

---
+++ src/WebApplication70/ControllerNavigationTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace WebApplication70
+{
+    public class ControllerNavigationTagHelper : TagHelper
+    {
+        [Activate]
+        private IUrlHelper UrlHelper { get; set; }
+
+        public Type ControllerType { get; set; }
+
+        public string Exclude { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+
+            var actionNames = ControllerType.GetTypeInfo().DeclaredMethods
+                .Where(methodInfo => methodInfo.IsPublic)
+                .Select(methodInfo => methodInfo.Name);
+
+            var controllerName = ControllerType.Name;
+
+            if (controllerName.EndsWith("Controller", StringComparison.OrdinalIgnoreCase))
+            {
+                controllerName = controllerName.Substring(0, controllerName.Length - "Controller".Length);
+            }
+
+            foreach (var name in actionNames)
+            {
+                if (!string.Equals(name, Exclude, StringComparison.OrdinalIgnoreCase))
+                {
+                    var displayName = 
+                        string.Equals(name, "Index", StringComparison.OrdinalIgnoreCase) ? controllerName : name;
+                    output.PostContent.Append($"<li><a href='{UrlHelper.Action(name, controllerName)}'>{displayName}</a></li>");
+                }
+            }
+        }
+    }
+}
---
+++ src/WebApplication70/VisibleTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace WebApplication70
+{
+    [TargetElement(Attributes = "asp-visible")]
+    public class VisibleTagHelper : TagHelper
+    {
+        public override int Order
+        {
+            get
+            {
+                return int.MaxValue;
+            }
+        }
+
+        public bool AspVisible { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!AspVisible)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
